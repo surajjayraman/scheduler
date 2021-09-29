780ed76b22dcbfa4e8205f92fb4cc87e420899b8
surajjayraman <suraj.jayraman@gmail.com>
2021-09-29 18:00:17 -0400

    Refactor InterviewerList component using values from props.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,12 +4,26 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 
 export default function InterviewerList(props){
+    //Refactor using values from props
+    const parsedInterviewersList = props.interviewers.map(interviewer => {
+  
+        return (
+          <InterviewerListItem 
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === props.value}
+            setInterviewer={ event => props.onChange(interviewer.id)}
+          />
+        );
+      })
+    
     
     return (
         <section className="interviewers">
         <h4 className="interviewers__header text--light">Interviewer</h4>
         <ul className="interviewers__list">
-        
+            {parsedInterviewersList}        
         </ul>
         </section>
     );
