a9f5b6e1c7d2a0e931d18a402fa1a0437234f13f
surajjayraman <suraj.jayraman@gmail.com>
2021-09-29 17:45:30 -0400

    Add stories to InterviewerList component.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
 
 
 export default function InterviewerList(props){
+    
     return (
         <section className="interviewers">
         <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list"></ul>
+        <ul className="interviewers__list">
+        
+        </ul>
         </section>
     );
 }
