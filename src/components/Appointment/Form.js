b2cf76c6f6aa93d3b06a89bba41981e4d490e04e
surajjayraman <suraj.jayraman@gmail.com>
2021-09-30 16:22:35 -0400

    Add stories for the Empty component of Appointment to render Show / Empty.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,13 +4,13 @@ import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
 export default function Form(props) {
-    const [name, setName] = useState(props.name || '');
+    const [name, setName] = useState(props.name || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
     const [error, setError] = useState("");
 
     // handle save and cancel
     function reset() {
-        setName('');
+        setName("");
         setInterviewer(null);
     }
 
