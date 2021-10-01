37df366b0ab13db5d39909da5e7322f8875c06ec
surajjayraman <suraj.jayraman@gmail.com>
2021-09-30 23:24:21 -0400

    Create helper fn selector getAppointmentsForDay.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+    // returns an array of appointments for the day
+    let results = [];
+  
+    // 1. finding the object in our state.days whos name matches day
+    for (let dayOfArr of state.days) {
+      if (dayOfArr.name === day ) {
+        const appointmentArray = [...dayOfArr.appointments];
+        // 2. access to appt array for the given day, we need to iterate through 
+        for(let appointment of appointmentArray) {
+          // and compare where its id matches the id of states.appointments and return that
+          if(state.appointments[appointment]) {
+            results.push(state.appointments[appointment])
+          }
+        }
+      }
+    }
+    // 3. validation - if no appointments on the given day, return empty []
+    return results;
+  
+  }
