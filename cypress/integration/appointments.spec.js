0895bca2cfa406f1c6165cd0856c03729c0051a3
surajjayraman <suraj.jayraman@gmail.com>
2021-10-06 19:00:37 -0400

    Add tests to the E2E appointment tests for student name input and choosing an interviewer.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -12,6 +12,9 @@ describe("Appointment", () => {
         .click();
       
       cy.get('[data-testid=student-name-input]')
-        .type('Lydia Miller-Jones');  
+        .type('Lydia Miller-Jones');
+        
+      cy.get('[alt="Sylvia Palmer"]')
+        .click();  
     });
 }); 
