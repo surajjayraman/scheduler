8c39e2463ab87e807ee4b9fd5bf06b44c45f5415
surajjayraman <suraj.jayraman@gmail.com>
2021-10-06 19:41:36 -0400

    Refactor E2E appointment tests: common beforeEach steps and then specific test steps.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,4 +1,4 @@
-describe("Appointment", () => {
+describe("Appointments", () => {
 
     beforeEach(() => {
       cy.request('GET','/api/debug/reset')
@@ -19,5 +19,8 @@ describe("Appointment", () => {
        
       cy.contains('Save')
         .click();  
+      
+      cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+      cy.contains('.appointment__card--show', 'Sylvia Palmer');
     });
 }); 
